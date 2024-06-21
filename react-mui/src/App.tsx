aad5a6a17daa5c11b0c4f610c1a39235c6026c09
Maheswar Mahapatra <maheswar2110@gmail.com>
2024-06-21 22:53:41 +0530

    done

--- react-mui/src/App.tsx
+++ react-mui/src/App.tsx
@@ -1,19 +1,41 @@
-import FontsInputs from "./sections/FontsInputs"
-import Layout from "./sections/Layout"
-import Navigation from "./sections/Navigation"
-import DataDisplay from "./sections/DataDisplay"
-import Feedback from "./sections/Feedback"
+import { createTheme, ThemeProvider, colors } from "@mui/material";
+import FontsInputs from "./sections/FontsInputs";
+import Layout from "./sections/Layout";
+import Navigation from "./sections/Navigation";
+import DataDisplay from "./sections/DataDisplay";
+import Feedback from "./sections/Feedback";
+import MuiResponsiveness from "./components/MuiResponsiveness";
+import { MuiCustomTheme } from "./components/MuiCustomTheme";
+
+const theme = createTheme({
+  status: {
+    danger: "#e53e3e",
+  },
+  palette: {
+    secondary: {
+      main: colors.orange[500],
+    },
+    neutral: {
+      main: colors.grey[500],
+      darker: colors.grey[700],
+    },
+  },
+});
 
 function App() {
   return (
-    <div>
-      {/* <FontsInputs /> */}
-      {/* <Layout /> */}
-      {/* <Navigation /> */}
-      {/* <DataDisplay /> */}
-      <Feedback />
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <div>
+        {/* <FontsInputs /> */}
+        {/* <Layout /> */}
+        {/* <Navigation /> */}
+        {/* <DataDisplay /> */}
+        {/* <Feedback /> */}
+        {/* <MuiResponsiveness /> */}
+        <MuiCustomTheme />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ react-mui/src/sections/MuiLabComponents.tsx
@@ -0,0 +1,17 @@
+import MuiLoadingButton from '../components/MuiLab Components/1MuiLoadingButton'
+import MuiTabs from '../components/MuiLab Components/2MuiLab'
+import MuiTimeline from '../components/MuiLab Components/3MuiTimeline'
+import MuiMasonry from '../components/MuiLab Components/4MuiMasonry'
+
+function MuiLabComponents() {
+  return (
+    <div>
+      {/* <MuiLoadingButton /> */}
+      {/* <MuiTabs /> */}
+      {/* <MuiTimeline /> */}
+      <MuiMasonry />
+    </div>
+  )
+}
+
+export default MuiLabComponents
