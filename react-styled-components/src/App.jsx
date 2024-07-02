0b7d381ccf0b9c31ec331fc20c4ea2f7e7c43230
Maheswar Mahapatra <maheswar2110@gmail.com>
2024-07-02 21:02:33 +0530

    done

--- react-styled-components/src/App.jsx
+++ react-styled-components/src/App.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import "./App.css";
+import logo from './assets/react.svg'
+import StyledButton, { FancyButton, SubmitButton } from "./components/Button/Button";
+import { AnimatedLogo, DarkButton } from './components/Button/Button.styles'
+
+// Theming
+const theme = {
+  dark: {
+    primary: '#000',
+    text: '#fff'
+  },
+  light: {
+    primary: '#fff',
+    text: '#000'
+  },
+  fontFamily: 'Segoe UI'
+}
+
+const GlobalStyle = createGlobalStyle`
+  button {
+    font-family: ${props => props.theme.fontFamily};
+  }
+`
 
 function App() {
   return (
-    <div>
-      App
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    <div className="App">
+      <StyledButton>Button 1</StyledButton>
+      <div>
+        <br />
+      </div>
+      <StyledButton variant="outline">Button 2</StyledButton>
+      <div>
+        <br />
+      </div>
+      <FancyButton as="a">Button 3</FancyButton>
+      <div>
+        <br />
+      </div>
+      <SubmitButton>Submit</SubmitButton>
+      <AnimatedLogo src={logo} />
+      <DarkButton>Dark Button</DarkButton>
     </div>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
